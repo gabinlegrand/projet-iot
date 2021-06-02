cf8ac1424479cf4ee3b6b83c6d049891c691c15b
Dorian Lamour <34211720+LDorian@users.noreply.github.com>
2021-06-02 15:01:26 +0200

    Update

--- mainSodaq.ino
+++ mainSodaq.ino
@@ -1,9 +1,6 @@
-#define DEBUG
 #include <Sodaq_RN2483.h>
 
-#define uplinkCnt 10
-
-#define debugSerial SerialUSB
+#define debugSerial  SerialUSB
 #define loraSerial  Serial2
 
 const uint8_t devEUI[8]  = {0x11, 0x22, 0x33, 0xFF, 0xD1, 0xD2, 0xD3, 0xD5} ;
@@ -17,16 +14,10 @@ void setup()
 {  
   debugSerial.begin(57600) ;
   loraSerial.begin(LoRaBee.getDefaultBaudRate()) ;
-
-  //Set the temperature sensor pin as input
-  pinMode(TEMP_SENSOR, INPUT) ;
-
-  //Set ADC resolution to 12 bits
-  analogReadResolution(12) ;
   
   delay(10000) ;
   
-  LoRaBee.setDiag(debugSerial) ; // optional
+  LoRaBee.setDiag(debugSerial) ;
  
     if (LoRaBee.initOTA(loraSerial, devEUI, appEUI, appKey, true))
     {
@@ -52,102 +43,25 @@ void setup()
 
 void loop()
 {
-  // Initialize voltage for temperature sensor
-  float sum = 0.0;
-  float voltage = 0.0;
-  float temperature = 0.0;
-  uint8_t samples = 10 ;
-  uint8_t payload[] = {0x00, 0x00, 0x00} ;
-  uint8_t data ;
-  
-  // Send x packets, with at least a 10 seconds delay after each transmission (more seconds if the device is busy)
-  uint8_t i = uplinkCnt ;
-  while (i > 0)
-  {
-    sum = 0.0 ;    // reset averaged reading to start at 0
-    // This loop does 10 readings of the voltage, and adds this to the variable
-    for(int i = 0; i < samples; i++)
-    {
-      sum += 3.3/4095.0 * (float)analogRead(TEMP_SENSOR) ;
-    }
+  char dnbuf[16];
+  char upbuf[16];
 
-    // Divide the voltage to get average
-    voltage = sum/samples ;
+  LoRaBee.getMacParam("dnctr", dnbuf, 16);
+  LoRaBee.getMacParam("upctr", upbuf, 16);
 
-    debugSerial.println(voltage, 1) ;
+  debugSerial.print("Downlink frame counter: ");
+  debugSerial.println(dnbuf);
+  debugSerial.print("Uplink frame counter: ");
+  debugSerial.println(upbuf);
 
-    // Convert voltage to temperature
-    temperature = (voltage - 0.5) * 100.0 ;
+  debugSerial.println("Sending message...");
 
-    // Print temperature to serial terminal
-    debugSerial.print(temperature, 1) ;
-    debugSerial.println(" C") ;
-
-    if (temperature >= 0)
-    {
-      payload[0] = '+' ;
-    }
-    else
-    {
-      payload[0] = '-' ;
-    }
-
-    data = (int)temperature ;
-    payload[2] = '0' + (data % 10) ;
-    data /= 10 ;
-    payload[1] = '0' + (data % 10) ;
-  
-    switch (LoRaBee.send(1, payload, 3))
-    {
-    case NoError:
-      debugSerial.println("Successful transmission.");
-
-  
-      delay(500) ;
-      
-      
-      i-- ;
-      break ;
-    case NoResponse:
-      debugSerial.println("There was no response from the device.") ;
-      break ;
-    case Timeout:
-      debugSerial.println("Connection timed-out. Check your serial connection to the device! Sleeping for 20sec.") ;
-      delay(20000) ;
-      break ;
-    case PayloadSizeError:
-      debugSerial.println("The size of the payload is greater than allowed. Transmission failed!") ;
-      break ;
-    case InternalError:
-      debugSerial.println("Oh No! This shouldn't happen. Something is really wrong! Try restarting the device!\r\nThe program will now halt.") ;
-      while (1) {} ;
-      break ;
-    case Busy:
-      debugSerial.println("The device is busy. Sleeping for 10 extra seconds.") ;
-      delay(10000) ;
-      break ;
-    case NetworkFatalError:
-      debugSerial.println("There is a non-recoverable error with the network connection. You should re-connect.\r\nThe program will now halt.") ;
-      while (1) {} ;
-      break ;
-    case NotConnected:
-      debugSerial.println("The device is not connected to the network. Please connect to the network before attempting to send data.\r\nThe program will now halt.") ;
-      while (1) {} ;
-      break ;
-    case NoAcknowledgment:
-      debugSerial.println("There was no acknowledgment sent back!") ;
-      break ;
-    default:
-      break ;
-    }
-   
-    delay(10000) ;
-  }
+  uint8_t buf[] = {'t', 'e', 's', 't'};
+  uint8_t i = LoRaBee.send(1, buf, sizeof(buf));
+  debugSerial.print("LoRa send: ");
+  debugSerial.println(i);
+  debugSerial.println("Need wait 5 seconds...");
+  delay(5000);
 
   debugSerial.println("End!");
-
-  //LoRaBee.sleep() ;
- 
-  while (1) 
-  { } // block forever
 }
