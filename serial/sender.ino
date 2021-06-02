12cb76d4f89686c539d9bd8edb0125b4beb70a83
Dorian Lamour <34211720+LDorian@users.noreply.github.com>
2021-06-02 16:02:56 +0200

    Update Sender

--- serial/sender.ino
+++ serial/sender.ino
@@ -1,15 +1,37 @@
 #include <SoftwareSerial.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+#define RST_PIN         9
+#define SS_PIN          10
 
-char mystr[10] = "Coucou"; //String data
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+SoftwareSerial mySerial(6, 7); // RX, TX
 
+unsigned long UID = 0;
+char buf[16];
 void setup() {
   // Begin the Serial at 57600 Baud
   mySerial.begin(57600);
+  Serial.begin(9600);   // Initialize serial communications with the PC
+  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 void loop() {
-  mySerial.write(mystr,10); //Write the serial data
-  delay(1000);
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  if (*((unsigned long *)mfrc522.uid.uidByte) != UID) {
+    UID = *((unsigned long *)mfrc522.uid.uidByte); //recup UID  unsigned long
+    ltoa(UID, buf, 10);
+    Serial.println(buf);
+    mySerial.write(buf, 10);
+  }
 }
