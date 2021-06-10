8bdbb90db69456779be6aee35c4ec91803f3f457
Dorian Lamour <34211720+LDorian@users.noreply.github.com>
2021-06-10 11:55:56 +0200

    Update mainSodaq.ino

--- mainSodaq.ino
+++ mainSodaq.ino
@@ -1,36 +1,49 @@
 #include <Sodaq_RN2483.h>
+#include <Servo.h>
 
 #define debugSerial  SerialUSB
 #define loraSerial  Serial2
 
-const uint8_t devEUI[8]  = {0x11, 0x22, 0x33, 0xFF, 0xD1, 0xD2, 0xD3, 0xD5} ;
+const uint8_t devEUI[8]  = {0x11, 0x22, 0x33, 0xFF, 0xD1, 0xD2, 0xD3, 0xD0} ;
 const uint8_t appEUI[8]  = {0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x04, 0x31, 0xD8} ;
-const uint8_t appKey[16] = {0xAF, 0xCF, 0x06, 0x67, 0x06, 0x72, 0xD9, 0x2D, 0x31, 0x98, 0xD6, 0xE0, 0x42, 0xF0, 0xE4, 0x5E} ;
+const uint8_t appKey[16] = {0xAF, 0xCF, 0x06, 0x67, 0x06, 0x72, 0xD9, 0x2D, 0x31, 0x98, 0xD6, 0xE0, 0x42, 0xF0, 0xE4, 0x50} ;
 
 // Payload is 'Microchip ExpLoRa' in HEX
 const uint8_t testPayload[] = {0x4d, 0x69, 0x63, 0x72, 0x6f, 0x63, 0x68, 0x69, 0x70, 0x20, 0x45, 0x78, 0x70, 0x4c, 0x6f, 0x52, 0x61} ;
 
 char rxStr[50];
+String list[20];
+int listCpt = 0;
+
+Servo servoLeft;
+Servo servoRight;
+
+int pos = 0;
 
 void setup()
-{  
+{
+  servoLeft.attach(3);
+  servoRight.attach(4);
+  servoLeft.write(5);
+  servoRight.write(45);
+
   debugSerial.begin(57600);
   Serial.begin(57600);
   loraSerial.begin(LoRaBee.getDefaultBaudRate()) ;
-  
+
   delay(10000) ;
-  
+
   LoRaBee.setDiag(debugSerial) ;
- 
-    if (LoRaBee.initOTA(loraSerial, devEUI, appEUI, appKey, true))
-    {
-      debugSerial.println("OTAA Keys Accepted.") ;
-    }
-    else
-    {
-      debugSerial.println("OTAA Keys Setup Failed!") ;
-    }
-  
+
+  if (LoRaBee.initOTA(loraSerial, devEUI, appEUI, appKey, true))
+  {
+    debugSerial.println("OTAA Keys Accepted.") ;
+  }
+  else
+  {
+    debugSerial.println("OTAA Keys Setup Failed!") ;
+  }
+
   debugSerial.println("Sleeping for 5 seconds before starting sending out test packets.");
   for (uint8_t i = 5; i > 0; i--)
   {
@@ -76,12 +89,30 @@ void downlink()
   uint16_t len = LoRaBee.receive(payload, 64);
   String HEXPayload = "";
   if (payload[0] != 131)
-  { 
-    for (int i = 0; i < len; i++)
+  {
+    HEXPayload += " ";
+    for (int i = 0; i < len-1; i++)
     {
       HEXPayload += String(payload[i], DEC);
     }
+
     debugSerial.println(HEXPayload);
+
+    if (HEXPayload == "111112101110") {
+      servoMotor();
+      return;
+    }
+
+    for (int i = 0; i < listCpt + 1; i ++)
+    {
+      debugSerial.println(list[i]);
+      if (HEXPayload == list[i]) {
+        debugSerial.println("it exist");
+        return;
+      }
+    }
+    list[listCpt] = HEXPayload;
+    listCpt++;
   }
   else
   {
@@ -89,11 +120,25 @@ void downlink()
   }
 }
 
-void rxtx(){
+void servoMotor() {
+  servoLeft.write(65);
+  servoRight.write(5);
+  delay(2000);
+}
+
+void rxtx() {
   if (Serial.available() > 0)
+  {
+    Serial.readBytes(rxStr, 20);
+    debugSerial.println(rxStr);
+    for (int i = 0; i < listCpt + 1; i ++)
     {
-      Serial.readBytes(rxStr, 20);
       debugSerial.println(rxStr);
-      delay(1000);
+      if (String(rxStr) == list[i]) {
+        servoMotor();
+        return;
+      }
     }
+    delay(1000);
+  }
 }
